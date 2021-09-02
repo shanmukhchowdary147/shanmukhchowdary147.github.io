86a50569c9cd41b2be07f60a37df9f9c86aea90a
shanmukhchowdary147 <57340285+shanmukhchowdary147@users.noreply.github.com>
2021-09-02 21:42:27 +0530

    eduaction details

--- src/components/body/work/index.js
+++ src/components/body/work/index.js
@@ -10,8 +10,14 @@ function Work() {
       <Separator />
       <label className="section-title">Education</label>
       <div className="educat">
-        <h1 >Bachelor's Degree in Computer Science & Engineering </h1>
+        <h1 >Bachelor of Technology, Computer Science </h1>
         <p>Bennett university (2019-2023), cureent CGPA: 8.14/10 </p>
+        <h1 >Intermediate, MPC </h1>
+        <p>Board Of Intermediate Education Andhra Pradesh </p>
+        <p>Sri Chaiatanya junior colege (2017-2019), CGPA: 9.94/10 </p>
+        <h1 >Class X </h1>
+        <p>Board Of Secondary Education Andhra Pradesh </p>
+        <p>ST.Claret EM High School (2017), GPA: 10/10 </p>
       </div>
       <div >
       <h1 >Certifications </h1>
