907287c50ce2449bb0c1366bb0fa5f099fbd78df
shanmukhchowdary147 <57340285+shanmukhchowdary147@users.noreply.github.com>
2021-09-03 10:33:54 +0530

    rel

--- src/components/common/social-contact/index.js
+++ src/components/common/social-contact/index.js
@@ -7,7 +7,7 @@ function SocialContact() {
     <div className="social-contact">
       {data.map((item) => {
         return (
-          <a href={item.link} target="_blank">
+          <a href={item.link} target="_blank" rel="noreferrer">
             <div className="social-icon-div">
               <img src={item.icon} className="social-icon" />
             </div>
