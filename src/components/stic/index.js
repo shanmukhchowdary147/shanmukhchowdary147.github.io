09110ab4e0c70ee5f6b57ef807e52142bb83999d
shanmukhchowdary147 <57340285+shanmukhchowdary147@users.noreply.github.com>
2021-09-12 20:02:37 +0530

    h

--- src/components/stic/index.js
+++ src/components/stic/index.js
@@ -5,6 +5,9 @@ import { FaGithub, FaTwitter,FaFacebookF, FaLinkedin } from "react-icons/fa";
 import x from '.'
 
 export default function Socialicons() {
+  const emailClick=()=>{
+    window.location.assign('mailto:shanmukhchowdary147@gmail.com');
+  };
   return (
     <div>
       <div className="stick_follow_icon">
@@ -27,7 +30,7 @@ export default function Socialicons() {
               style={{ height: 50, width: 50 }}
             />
           </li>
-          <li>
+          {/* <li>
             <SocialIcon
               url="mailto:shanmukhchowdary147@gmail.com"
               bgColor="#01152b"
@@ -35,7 +38,7 @@ export default function Socialicons() {
               target="_blank"
               style={{ height: 50, width: 50 }}
             />
-          </li>
+          </li> */}
           <li>
             <SocialIcon
               url="https://www.instagram.com/___shannu.___/"
@@ -57,8 +60,8 @@ export default function Socialicons() {
         </ul>
         <img src={require("../../assets/image/social-media-arrow.svg").default} alt="" className="imagelineL" />            
       </div>
-      <div className="emailright" onClick="location.href = 'www.yoursite.com';" >
-        {/* <p>shanmukhchowdary147@gmail.com</p> */}
+      <div className="emailright" onClick={emailClick} >
+        <p >shanmukhchowdary147@gmail.com</p>
         {/* <img src={require("../../assets/image/scroll-down-arrow.svg").default} alt="" className="imageLineR" />             */}
       </div>
     </div>
