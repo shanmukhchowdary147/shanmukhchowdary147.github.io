fb6d7886e1a8b9f5c8dc014b8fe36def626d668a
shanmukhchowdary147 <57340285+shanmukhchowdary147@users.noreply.github.com>
2021-10-28 19:07:38 +0530

    splash

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/home/index";
+import Splash from "./components/splash";
 
 function App() {
-  return <Home />;
+  return(
+    <div>
+      <Splash />
+      <Home />
+    </div>
+  )
 }
 
 export default App;
