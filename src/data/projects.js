bbfd5c470698333ca1cb075bab7470118a59c245
Shanmukh Chowdary <57340285+shanmukhchowdary147@users.noreply.github.com>
2021-12-11 22:20:50 +0530

    wildfire git

--- src/data/projects.js
+++ src/data/projects.js
@@ -31,7 +31,7 @@ export const ProjectData = [
     tags: [
       "Python"
     ],
-    // github: "https://github.com/shanmukhchowdary147/Farm_Share",
+    github: "https://github.com/shanmukhchowdary147/Wildfire-Prediction",
     image:
       "https://cdn.dribbble.com/users/2657627/screenshots/13959270/media/b1a8b2b53760dc5c10e6cddc1409a3a1.png?compress=1&resize=400x300",
   },
