6fb76637e11cb36698956a17ee658b8ec85484f7
Shanmukh Chowdary <57340285+shanmukhchowdary147@users.noreply.github.com>
2021-12-11 21:01:40 +0530

    projects update

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export const ProjectData = [
     id: 1,
     title: "ForHer",
     about:
-      "ForHer is a women safety app where women can send their current location to their guardians and police when they feel insecure and in danger. They just need to open the app and send the alert message to their family or friends just by shaking the phone or clicking the send location button. There is a separate Login page for police to see the recent requests that contain the user's image, name, location, and contact number",
+      "ForHer is a women safety app where women can send their current location to their guardians and police when they feel insecure and in danger just by shaking the phone or clicking the send location button. There is a separate Login page for police to see the recent requests that contain the user's image, name, location, and contact number.",
     tags: ["Android Studio", "Java", "Google Firebase"],
     github: "https://github.com/shanmukhchowdary147/ForHer",
     image:
@@ -15,7 +15,7 @@ export const ProjectData = [
     id: 1,
     title: "Farm Share",
     about:
-      "This is a project to help farmers overcome the problem of lack of machinery or agricultural equipment. Lessee ( farmers who cannot afford to buy agricultural equipment) can take equipment as rent from Lessor ( farmers who can afford to buy equipment) and pay them. So, in this way both the farmers get benefited.",
+      "Farm Share is an android app developed to help farmers overcome the problem of lack of machinery or agricultural equipment. Lessee ( farmers who cannot afford to buy agricultural equipment) can take equipment as rent from Lessor ( farmers who can afford to buy equipment) and pay them. So, in this way both the farmers get benefited.",
     tags: [
       "Android Studio", "Java", "Google Firebase"
     ],
@@ -25,9 +25,21 @@ export const ProjectData = [
   },
   {
     id: 1,
+    title: "WildFire Prediction",
+    about:
+      "Wildfire prediction is a machine learning project to predict wildfires in the near future.",
+    tags: [
+      "Python"
+    ],
+    // github: "https://github.com/shanmukhchowdary147/Farm_Share",
+    image:
+      "https://cdn.dribbble.com/users/2657627/screenshots/13959270/media/b1a8b2b53760dc5c10e6cddc1409a3a1.png?compress=1&resize=400x300",
+  },
+  {
+    id: 1,
     title: "Covi-19 Tracker App",
     about:
-      "This is an android app to track the Covid-19 cases. It shows some major aspects like total cases, active cases, today's cases, total deaths, today's deaths, number of people recovered, and affected countries. This information is divided into 2 particular sections so that the user can keep track of the total cases worldwide and in a particular country.",
+      "Covi-19 Tracker is an android app to track the status of Covid-19 cases globally and nationally. It shows some major aspects like total cases, active cases, today's cases, total deaths, today's deaths, number of people recovered, and affected countries.",
     tags: [
       "Android Studio", "Java", "Retrofit"
     ],
