e05476b09a2207e9a8bd65f2cee215acd62a0db5
Shanmukh Chowdary <57340285+shanmukhchowdary147@users.noreply.github.com>
2022-06-19 17:15:21 +0530

    DevOps projects updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,25 @@
 export const ProjectData = [
   {
     id: 1,
+    title: "packnDeploy",
+    about:
+      "packnDeploy is an automated CI-CD pipeline to deploy a web application. Everything is automated from Dockerizing the application, pushing the image to Docker Registry like AWS ECR, and deploying it to Elastic Beanstalk using AWS CodeBuild, CodeDeploy, and CodePipeline. Also written the YAML configuration to dockerize the application and push the Image to Docker Registry.",
+    tags: ["AWS", "Docker", "CI_CD Pipelines"],
+    github: "https://github.com/shanmukhchowdary147/packnDeploy",
+    image:
+      "https://cdn.dribbble.com/users/2657627/screenshots/13959270/media/b1a8b2b53760dc5c10e6cddc1409a3a1.png?compress=1&resize=400x300",
+  },
+  {
+    id: 1,
+    title: "push2deploy",
+    about:
+      "Created a complete CI-CD pipeline using Jenkins server to continuously deploy application version changes from GitHub to the production environment. Integrated AWS CodeBuild plugin and CodeDeploy plugin to Jenkins. Used the cloud formation to deploy all the necessary resources like EC2 instance running with Jenkins, AWS CodeBuild, CodeDeploy, S3 to store artifacts, etc.",
+    tags: ["AWS", "Jenkins"],
+    image:
+      "https://cdn.dribbble.com/users/2657627/screenshots/13959270/media/b1a8b2b53760dc5c10e6cddc1409a3a1.png?compress=1&resize=400x300",
+  },
+  {
+    id: 1,
     title: "ForHer",
     about:
       "ForHer is a women safety app where women can send their current location to their guardians and police when they feel insecure and in danger just by shaking the phone or clicking the send location button. There is a separate Login page for police to see the recent requests that contain the user's image, name, location, and contact number.",
@@ -47,15 +66,15 @@ export const ProjectData = [
     image:
       "https://cdn.dribbble.com/users/2657627/screenshots/13959270/media/b1a8b2b53760dc5c10e6cddc1409a3a1.png?compress=1&resize=400x300",
   },
-  {
-    id: 1,
-    title: "Hostel management system",
-    about:
-      "This is a Desktop application used to manage Hostel Administration.",
-    tags: [
-      "Java NetBeans", "MySQL"
-    ],
-    image:
-      "https://cdn.dribbble.com/users/2657627/screenshots/13959270/media/b1a8b2b53760dc5c10e6cddc1409a3a1.png?compress=1&resize=400x300",
-  },
+  // {
+  //   id: 1,
+  //   title: "Hostel management system",
+  //   about:
+  //     "This is a Desktop application used to manage Hostel Administration.",
+  //   tags: [
+  //     "Java NetBeans", "MySQL"
+  //   ],
+  //   image:
+  //     "https://cdn.dribbble.com/users/2657627/screenshots/13959270/media/b1a8b2b53760dc5c10e6cddc1409a3a1.png?compress=1&resize=400x300",
+  // },
 ];
