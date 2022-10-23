fbbcad1bd31e3c16c8a17cee1c7faae4d1d485db
Shanmukh Chowdary <57340285+shanmukhchowdary147@users.noreply.github.com>
2022-10-23 19:22:18 +0530

    profile added

--- src/components/body/work/index.js
+++ src/components/body/work/index.js
@@ -1,41 +1,46 @@
-import React from "react";
-import { WorkData } from "../../../data/work";
-import WorkCard from "./work-card";
-import "./work.css";
-import Separator from "../../common/separator/index";
+import React from 'react';
+import { WorkData } from '../../../data/work';
+import WorkCard from './work-card';
+import './work.css';
+import Separator from '../../common/separator/index';
 function Work() {
-  const picClick=()=>{
-    window.open("https://www.linkedin.com/in/shanmukhchowdary147/","_blank");
+  const picClick = () => {
+    window.open('https://www.linkedin.com/in/shanmukhchowdary147/', '_blank');
   };
   const data = WorkData;
   return (
     <div className="work">
       <Separator />
       <label className="section-title">About Me</label>
-      <br/> <br/>
+      <br /> <br />
       <div className="about">
         <div className="educat">
-          <h1 >Education </h1>
-          <p1 >Bachelor of Technology, Computer Science </p1> <br/>
-          <p2>Bennett University (2019-2023), Current CGPA: 8.5/10 </p2> <br/> <br/> 
-          <p1 >Intermediate, MPC </p1> <br/>
-          <p2>Board Of Intermediate Education Andhra Pradesh </p2> <br/>
-          <p2>Sri Chaiatanya Junior College (2017-2019), CGPA: 9.94/10 </p2> <br/> <br/>
-          <p1 >Class X </p1> <br/>
-          <p2>Board Of Secondary Education Andhra Pradesh </p2> <br/>
-          <p2>ST.Claret EM High School (2017), GPA: 10/10 </p2> <br/>
+          <h1>Education </h1>
+          <p1>Bachelor of Technology, Computer Science </p1> <br />
+          <p2>
+            Bennett University (2019-2023), Current CGPA: 8.5/10{' '}
+          </p2> <br /> <br />
+          <p1>Intermediate, MPC </p1> <br />
+          <p2>Board Of Intermediate Education Andhra Pradesh </p2> <br />
+          <p2>
+            Sri Chaiatanya Junior College (2017-2019), CGPA: 9.94/10{' '}
+          </p2>{' '}
+          <br /> <br />
+          <p1>Class X </p1> <br />
+          <p2>Board Of Secondary Education Andhra Pradesh </p2> <br />
+          <p2>ST.Claret EM High School (2017), GPA: 10/10 </p2> <br />
         </div>
         <div className="about-pic">
-          <br/> <br/> <br/>
-            <img
-              src={require("../../../assets/image/pf.jpg").default}
-              className="pic"
-              onClick={picClick}
-            />
+          <br /> <br /> <br />
+          <img
+            src={require('../../../assets/image/profile6.png').default}
+            className="pic"
+            onClick={picClick}
+          />
         </div>
       </div>
-      <div >
-      <h1 >Certifications </h1>
+      <div>
+        <h1>Certifications </h1>
       </div>
       <div className="work-list">
         {data.map((item) => {
